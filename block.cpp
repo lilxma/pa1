cab47152e74a0c0efee55fd7c39712aebfeef01d
UBC Student <lindxma@thetis.students.cs.ubc.ca>
2020-10-03 22:08:17 -0700

    added flip

--- block.cpp
+++ block.cpp
@@ -8,9 +8,9 @@
 //in each row how many HSLAPixels
 int Block::width() const
 {
-    if (data.size() == 0) {
-        return 0;
-    }
+    // if (data.size() == 0) {
+    //     return 0;
+    // }
     return data[0].size();
 }
 
@@ -24,6 +24,17 @@ int Block::height() const
 //render takes in an image and some parameters and renders(draws) the current block onto the given image.
 void Block::render(PNG &im, int column, int row) const
 {
+//     if (!(column>=0 && (unsigned)column<im.width())) return;
+//     if (!(row>=0 && (unsigned)row<im.width())) return;
+  
+//   int col_bound = (column + width() > (int)im.width())? im.width() : (column + width());
+//   int row_bound = (row + height() > (int)im.height())? im.height() : (row + height());
+  
+//   for (int col = column; col<col_bound; col++){
+//     for (int rowNum = row; rowNum<row_bound; rowNum++){
+//       *(im.getPixel(col, rowNum)) = data[rowNum-(unsigned)row][col-(unsigned)column];
+//     }
+//   }
     int c = 0;
     int r = 0;
 
@@ -36,39 +47,24 @@ void Block::render(PNG &im, int column, int row) const
             }
         }
     }
-    c++;
-}
-
-    // for (int x = column; x < column + width(); x++) {
-    //     for (int y = row; y < row + height(); y++) {
-    //         if ((unsigned) x < im.width() && (unsigned) y < im.height()) {
-    //              HSLAPixel *pixelImage = im.getPixel(x, y);
-    //              for (int i = 0; i < width(); i++) {
-    //                 for (int j = 0; j < height(); j++) {
-    //                  *pixelImage = data[i][j];
-    //                 }
-    //             }        
-    //         }
-            
-    //     }
-    // }
-}
+        c++;
+    }
+}   
 
 //You will be taking the pixels from a rectangular region in an image, and placing them into a block. 
 //A block is just a vector whose elements are vectors of pixels.
 void Block::build(PNG &im, int column, int row, int width, int height)
 {
-    // int c = 0;
-    // int r = 0;
+
+    // unsigned int col_bound = column + width;
+    // unsigned int row_bound = row + height;
 
     // data.resize(height);
-    // for (int col = column; col < column + width; col++) {
-    //     data[col].resize(width);
-    //     for (int rowNum = row; rowNum < row + height; rowNum++){
-    //         data[c][r] = *(im.getPixel(col,rowNum));
-    //         r++;
+    // for (unsigned int rowNum=row; rowNum<row_bound; rowNum++){
+    //     data[rowNum].resize(width);
+    //     for (unsigned int col=column; col<col_bound; col++){
+    //     data[rowNum - row][col-column] = *(im.getPixel(col, rowNum));
     //     }
-    //     c++;
     // }
 
     vector<HSLAPixel> pixels;
@@ -85,8 +81,10 @@ void Block::flip()
 {
 int last = height()-1;
 for (int i = 0; i < height()/2; i++) {
-  for (int j = 0; j < width(); j++) {
-     data[j][i] = data[j][last];
+    for (int j = 0; j < width(); j++) {
+        HSLAPixel temp = data[j][i];
+        data[j][i] = data[j][last];
+        data[j][last] = temp;
     }
     last--;
     }
